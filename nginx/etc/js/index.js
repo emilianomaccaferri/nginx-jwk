b8f633e34fc79200b7c86640cbc958b3b5225996
Emiliano Maccaferri <donglerio@unimore.it>
2025-01-22 16:13:23 +0100

    add: poc works!

--- nginx/etc/js/index.js
+++ nginx/etc/js/index.js
@@ -1,3 +1,16 @@
+const encoder = new TextEncoder();
+const decode = (encoded) => {
+  encoded = encoded.replace(/-/g, '+').replace(/_/g, '/').replace(/\s/g, '');
+  return decodeBase64(encoded);
+}
+const decodeBase64 = (encoded) => {
+  const binary = atob(encoded);
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < binary.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return bytes;
+};
 const validateJwt = async (res) => {
   try {
     // get the jwk (which can be cached)
@@ -6,7 +19,7 @@ const validateJwt = async (res) => {
     const preferred_key = parsed_jwk["keys"]
       .filter(k =>
         k.use === "sig"
-        && k.alg === "RS256"
+        && k.alg === "ES512"
       );
 
     if (preferred_key.length === 0)
@@ -19,26 +32,29 @@ const validateJwt = async (res) => {
         "jwk",
         preferred_key[0],
         {
-          name: "RSA-OAEP",
-          hash: "SHA-256"
+          name: "ECDSA",
+          namedCurve: "P-521"
         },
         true,
-        ["encrypt"]
+        ["verify"]
       );
     const jwt_split = jwt.split(".");
-    if (jwt.split.length !== 3) {
+    if (jwt_split.length !== 3) {
       res.variables.no_auth_reason = "invalid_jwt";
       res.return(401);
       return;
     }
-    const [header, payload, signature] = jwt_split;
+    const signing_input = jwt_split.slice(0, 2).join('.');
     const verify = await crypto.subtle.verify({
-      name: "RSA-OAEP",
+      name: "ECDSA",
+      namedCurve: "P-521",
+      hash: "SHA-512",// from the header
     },
       key,
-      signature,
-      data
+      decode(jwt_split[2]),
+      encoder.encode(signing_input),
     );
+    res.error(verify);
     res.return(203);
   } catch (err) {
     res.error(err);
